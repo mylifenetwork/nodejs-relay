c5ceb2d7d57ac92ef209ead899f98160931e2e33
nongdingzhan <21429545@life.hkbu.edu.hk>
2023-01-17 13:50:41 +0800

    test vercel

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./api/index');
-var usersRouter = require('./api/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
