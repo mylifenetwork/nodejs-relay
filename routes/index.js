efc3a8fd2fb316b2d64f6498898dc538b3623798
nongdingzhan <21429545@life.hkbu.edu.hk>
2023-03-23 12:20:26 +0800

    value change

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var router = express.Router();
-const http = require('http');
-var bodyParser = require('body-parser');
+let express = require('express');
+let router = express.Router();
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+
+let bodyParser = require('body-parser');
 /* GET home page. */
 
 
@@ -9,17 +11,29 @@ const webSocketServer = require('websocket').server;
 const ServerPort = 3001;
 
 const app = express();
-app.use(bodyParser.json({limit: '500mb'}));
-app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+  else  next();
+});
+// app.use(bodyParser.json({limit: '500mb'}));
+// app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 var jsonParser = bodyParser.json()
-
+let map=new Map()
+let tasklist=[]
+let taskmap=new Map()
+let resultmap=new Map()
+let taskidpointer=0
 
 app.get('/', function(req, res, next) {
   res.send('启动成功1');
 });
 
 // 创建应用服务器
-const server = http.createServer(app);
+/*const server = http.createServer(app);
 
 // 启动Http服务
 server.listen(ServerPort, function() {
@@ -65,19 +79,22 @@ wss.on('request', function(request) {
   clients[userID] = connection;
   console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
 });
+*/
 
-var map=new Map()
-var resultmap=new Map()
 app.post('/ajax/sendmodel',jsonParser,(request, response) => {
   // 设置响应头  设置允许跨域
   response.setHeader('Accss-Control-Allow-Origin', '*');
   //console.log(request.body)
   var model=request.body.model;
   var dataset=request.body.dataset;
-  var id=request.body.id;
+  //var id=request.body.id;
+  console.log(request.session.user)
+  var id=request.session.user.id;
+  request.body.id=id.toString();
   var type=request.body.type;
-  map.set(id,request.body)
-  console.log(model)
+  //map.set(id,request.body)
+  
+  //console.log(model)
   //console.log(dataset)
   console.log(id)
   if(type=="lgbm")
@@ -92,41 +109,188 @@ app.post('/ajax/sendmodel',jsonParser,(request, response) => {
     resultmap.delete(id+"_nncol")
   }
  
-  GPUws.send("id#"+id)
+  //GPUws.send("id#"+id)
+  if(taskidpointer>200)
+  {
+    taskmap.delete((taskidpointer-200));
+  }
+  taskidpointer=taskidpointer+1;
+  taskmap.set(taskidpointer,request.body)
+  tasklist.push({taskid:taskidpointer,userid:id})
+  //console.log(taskmap)
   // 设置响应体
-  response.send("success");
+  return response.send({status:"success",taskid:taskidpointer});
 
 });
 app.post('/ajax/sendmodelcol',jsonParser,(request, response) => {
-  // 设置响应头  设置允许跨域
+  try {
+     // 设置响应头  设置允许跨域
   response.setHeader('Accss-Control-Allow-Origin', '*');
   console.log(request.body)
-  var id=request.body.id;
+  //var id=request.body.id;
+  var id=request.session.user.id;
+  request.body.id=id.toString();
+  var maintaskid=request.body.maintaskid;
   var type=request.body.type;
-  var modeldata=map.get(id);
+  var modeldata=taskmap.get(maintaskid);
   modeldata.selectvalue=request.body.selectvalue;
   modeldata.colid=request.body.colid;
   modeldata.idcolid=request.body.idcolid;
   modeldata.labelcolid=request.body.labelcolid;
   modeldata.type=type;
   //console.log(dataset)
-  resultmap.delete(id+"_lgbmcol")
-  resultmap.delete(id+"_nncol")
-  console.log(id)
-  GPUws.send("id#"+id)
+  // resultmap.delete(id+"_lgbmcol")
+  // resultmap.delete(id+"_nncol")
+  console.log(request.body)
+  //GPUws.send("id#"+id)
   // 设置响应体
-  response.send("success");
+  if(taskidpointer>200)
+  {
+    taskmap.delete((taskidpointer-200));
+  }
+  taskidpointer=taskidpointer+1;
+  taskmap.set(taskidpointer,modeldata)
 
+  tasklist.push({taskid:taskidpointer,userid:id})
+  console.log(taskmap)
+  response.send({status:"success",taskid:taskidpointer});
+    
+  } catch (error) {
+    console.log(error)
+  }
+ 
+});
+app.post('/ajax/sendmodelrow',jsonParser,(request, response) => {
+  try {
+     // 设置响应头  设置允许跨域
+  response.setHeader('Accss-Control-Allow-Origin', '*');
+  console.log(request.body)
+  //var id=request.body.id;
+  var id=request.session.user.id;
+  request.body.id=id.toString();
+  var maintaskid=request.body.maintaskid;
+  var type=request.body.type;
+  var modeldata=taskmap.get(maintaskid);
+  modeldata.jsondata=request.body.jsondata;
+  modeldata.colid=request.body.colid;
+  modeldata.idcolid=request.body.idcolid;
+  modeldata.labelcolid=request.body.labelcolid;
+  modeldata.type=type;
+  //console.log(dataset)
+  // resultmap.delete(id+"_lgbmcol")
+  // resultmap.delete(id+"_nncol")
+  console.log(request.body)
+  //GPUws.send("id#"+id)
+  // 设置响应体
+  if(taskidpointer>200)
+  {
+    taskmap.delete((taskidpointer-200));
+  }
+  taskidpointer=taskidpointer+1;
+  taskmap.set(taskidpointer,modeldata)
+
+  tasklist.push({taskid:taskidpointer,userid:id})
+  console.log(taskmap)
+  response.send({status:"success",taskid:taskidpointer});
+    
+  } catch (error) {
+    console.log(error)
+  }
+ 
+});
+app.post('/ajax/sendmodelp',jsonParser,(request, response) => {
+  try {
+     // 设置响应头  设置允许跨域
+  response.setHeader('Accss-Control-Allow-Origin', '*');
+  console.log(request.body)
+  //var id=request.body.id;
+  var id=request.session.user.id;
+  request.body.id=id.toString();
+  var maintaskid=request.body.maintaskid;
+  var type=request.body.type;
+  var modeldata=taskmap.get(maintaskid);
+  //modeldata.jsondata=request.body.jsondata;
+  // modeldata.colid=request.body.colid;
+  modeldata.idcolid=request.body.idcolid;
+  modeldata.cidx=request.body.cidx;
+  modeldata.labelcolid=request.body.labelcolid;
+  modeldata.type=type;
+  //console.log(dataset)
+  // resultmap.delete(id+"_lgbmcol")
+  // resultmap.delete(id+"_nncol")
+  console.log(request.body)
+  //GPUws.send("id#"+id)
+  // 设置响应体
+  if(taskidpointer>200)
+  {
+    taskmap.delete((taskidpointer-200));
+  }
+  taskidpointer=taskidpointer+1;
+  taskmap.set(taskidpointer,modeldata)
+
+  tasklist.push({taskid:taskidpointer,userid:id})
+  console.log(taskmap)
+  response.send({status:"success",taskid:taskidpointer});
+    
+  } catch (error) {
+    console.log(error)
+  }
+ 
+});
+app.post('/ajax/sendmodelpl',jsonParser,(request, response) => {
+  try {
+     // 设置响应头  设置允许跨域
+  response.setHeader('Accss-Control-Allow-Origin', '*');
+  console.log(request.body)
+  //var id=request.body.id;
+  var id=request.session.user.id;
+  request.body.id=id.toString();
+  var maintaskid=request.body.maintaskid;
+  var type=request.body.type;
+  var modeldata=taskmap.get(maintaskid);
+  modeldata.jsondata=request.body.jsondata;
+  if(request.body.jsondata2!=undefined)
+    modeldata.jsondata2=request.body.jsondata2;
+  //modeldata.colid=request.body.colid;
+  modeldata.cidx=request.body.cidx;
+  modeldata.idcolid=request.body.idcolid;
+  modeldata.labelcolid=request.body.labelcolid;
+  modeldata.type=type;
+  //console.log(dataset)
+  // resultmap.delete(id+"_lgbmcol")
+  // resultmap.delete(id+"_nncol")
+  console.log(request.body)
+  //GPUws.send("id#"+id)
+  // 设置响应体
+  if(taskidpointer>200)
+  {
+    taskmap.delete((taskidpointer-200));
+  }
+  taskidpointer=taskidpointer+1;
+  taskmap.set(taskidpointer,modeldata)
+
+  tasklist.push({taskid:taskidpointer,userid:id})
+  console.log(taskmap)
+  response.send({status:"success",taskid:taskidpointer});
+    
+  } catch (error) {
+    console.log(error)
+  }
+ 
 });
 app.get('/ajax/checkresult',jsonParser,(request, response) => {
   // 设置响应头  设置允许跨域
   response.setHeader('Accss-Control-Allow-Origin', '*');
   //console.log(request.query)
   var params = request.query
-  var userid=params.userid;
+  //var userid=params.userid;
+  var userid=request.session.user.id;
+  request.body.id=userid;
+  var taskid=parseInt(params.taskid);
   var type=params.type;
-  console.log(userid+"_"+type)
-  var modeldata=map.get(userid)
+  console.log(userid+"_"+type+"_"+taskid)
+  //var modeldata=map.get(userid)
+  var modeldata=taskmap.get(taskid)
   if(modeldata==null)
   {
     response.json({status:"empty"})
@@ -135,17 +299,25 @@ app.get('/ajax/checkresult',jsonParser,(request, response) => {
   {
     var resultdata=null;
     if(type=="nn")
-      resultdata=resultmap.get(userid+"_nn")
+      resultdata=resultmap.get(taskid+"_nn")
     else if(type=="lgbm")
-      resultdata=resultmap.get(userid+"_lgbm")
+      resultdata=resultmap.get(taskid+"_lgbm")
     else if(type=="lgbmcol")
-      resultdata=resultmap.get(userid+"_lgbmcol")
+      resultdata=resultmap.get(taskid+"_lgbmcol")
+    else if(type=="lgbm_p")
+      resultdata=resultmap.get(taskid+"_lgbm_p")
+    else if(type=="lgbm_pl")
+      resultdata=resultmap.get(taskid+"_lgbm_pl")
     else if(type=="nncol")
-      resultdata=resultmap.get(userid+"_nncol")
+      resultdata=resultmap.get(taskid+"_nncol")
+    else if(type=="lgbmrow")
+      resultdata=resultmap.get(taskid+"_lgbmrow")
+    else if(type=="nnrow")
+      resultdata=resultmap.get(taskid+"_nnrow")
     //console.log(resultdata)
     if(resultdata!=null)
       response.json(resultdata)
-    else if(resultdata==null&&modeldata!=null&&modeldata.type==type)
+    else if(resultdata==null&&modeldata!=null&&modeldata.running==1)
       response.json({status:"running"});
     else
       response.json({status:"empty"});
@@ -155,52 +327,212 @@ app.get('/ajax/checkresult',jsonParser,(request, response) => {
 
 });
 
+app.get('/ajax/getrecordlist',jsonParser,async (request, response) => {
+  // 设置响应头  设置允许跨域
+  response.setHeader('Accss-Control-Allow-Origin', '*');
+  //console.log(request.query)
+  var params = request.query
+  //var userid=params.userid;
+  var type=params.type;
+  try {
+    var userid=request.session.user.id;
+    const records=await getResultList(userid,type);
+    response.json(records);
+  } catch (error) {
+    console.log(error)
+  }
+ 
+
+});
+app.get('/ajax/getdetailrecord',jsonParser,async (request, response) => {
+  // 设置响应头  设置允许跨域
+  response.setHeader('Accss-Control-Allow-Origin', '*');
+  //console.log(request.query)
+  var params = request.query
+  //var userid=params.userid;
+  var recordid=params.recordid;
+  try {
+    var userid=request.session.user.id;
+    const records=await getDetailResult(userid,recordid);
+    response.json(records);
+  } catch (error) {
+    console.log(error)
+  }
+
+
+});
+
 app.get('/py/getdata',jsonParser,(request, response) => {
   // 设置响应头  设置允许跨域
   response.setHeader('Accss-Control-Allow-Origin', '*');
   console.log(request.query)
   var params = request.query
   userid=params.userid;
+  //var userid=req.session.user.id;
+  var taskid=parseInt(params.taskid);
   console.log(userid)
-  var modeldata=map.get(userid)
+  var modeldata=taskmap.get(taskid)
   // 设置响应体
   console.log(modeldata)
   if(modeldata!=null)
+  {
     response.json(modeldata)
+    modeldata["running"]=1
+  }
   else
     response.json({status:"failed"})
 
 });
-app.post('/py/returndata',jsonParser,(request, response) => {
+
+
+app.get('/py/polling',jsonParser,(request, response) => {
   // 设置响应头  设置允许跨域
   response.setHeader('Accss-Control-Allow-Origin', '*');
-  console.log(request.body)
+  console.log(request.query)
+  var pytaskid = parseInt(request.query.taskid);
+  var count = parseInt(request.query.count);
+  console.log("taskid:"+taskidpointer+"  pytaskid:"+pytaskid)
+  if(count==0)
+  {
+    return response.json({taskid:taskidpointer,userid:"0"})
+  }
+  if((taskidpointer>0)&&(taskidpointer>pytaskid))
+  {
+    //console.log(tasklist)
+    response.json({taskid:pytaskid+1,userid:taskmap.get(pytaskid+1)["id"]})
+  }
+  else
+  {
+    response.json({taskid:taskidpointer,userid:"0"})
+  }
+  
+
+});
+app.post('/py/returndata',jsonParser,(request, response) => {
+  try {
+      // 设置响应头  设置允许跨域
+  response.setHeader('Accss-Control-Allow-Origin', '*');
+  console.log(request)
   var params = request.body
-  userid=params.userid;
-  console.log(userid)
-  var modeldata=map.get(userid)
-  modeldata.running=0;
+  var userid=params.userid;
+  var taskid=parseInt(params.taskid)
+  console.log(userid+","+taskid)
+  var modeldata=taskmap.get(taskid)
+  // console.log(modeldata)
+  modeldata["running"]=0;
   if(request.body.type=="nn")
   {
-    resultmap.set(userid+"_nn",request.body)
+    resultmap.set(taskid+"_nn",request.body)
+    if(request.body.error==undefined)
+      saveResult(userid,request.body.type,request.body)
   }
   else if(request.body.type=="lgbm")
   {
-    resultmap.set(userid+"_lgbm",request.body)
+    resultmap.set(taskid+"_lgbm",request.body)
+    if(request.body.error==undefined)
+      saveResult(userid,request.body.type,request.body)
   }
   else if(request.body.type=="lgbmcol")
   {
-    resultmap.set(userid+"_lgbmcol",request.body)
+    resultmap.set(taskid+"_lgbmcol",request.body)
   }
   else if(request.body.type=="nncol")
   {
-    resultmap.set(userid+"_nncol",request.body)
+    resultmap.set(taskid+"_nncol",request.body)
   }
-
+  else if(request.body.type=="lgbmrow")
+  {
+    resultmap.set(taskid+"_lgbmrow",request.body)
+  }
+  else if(request.body.type=="lgbm_p")
+  {
+    resultmap.set(taskid+"_lgbm_p",request.body)
+  }
+  else if(request.body.type=="lgbm_pl")
+  {
+    resultmap.set(taskid+"_lgbm_pl",request.body)
+  }
+  else if(request.body.type=="nnrow")
+  {
+    resultmap.set(taskid+"_nnrow",request.body)
+  }
   
   response.json({status:"success"})
+  } catch (error) {
+    console.log(error)
+  }
+
 
 });
+// Glassbox0128@
+const sequelize = new Sequelize('shapdatabase', 'root', 'Glassbox0128@', {
+  host: '127.0.0.1',
+  dialect: 'mysql',/* 选择 'mysql' | 'mariadb' | 'postgres' | 'mssql' 其一 */
+  timezone: '+08:00',
+  dialectOptions: {
+    dateStrings: true,
+    typeCast: true
+} 
+});
 
+const Record = sequelize.define('record', {
+  record_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  // 在这里定义模型属性
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  json_data: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  
+}, {
+  // 这是其他模型参数
+});
 
-module.exports = router;
+// `sequelize.define` 会返回模型
+console.log(Record === sequelize.models.Record);
+
+async function saveResult(userid,type,resultdata)
+{
+  const record = await Record.create({ record_id: 0,user_id:userid,type:type,json_data:JSON.stringify(resultdata)  });
+  console.log(record);
+}
+
+async function getResultList(userid,type)
+{
+  const records= await Record.findAll({
+    where: {
+      user_id: userid,
+      type:type
+    },
+    attributes: [
+      'record_id','user_id','type','createdAt','updatedAt'
+    ],
+    order: [
+      ['createdAt', 'DESC'],
+      
+  ]
+  });
+  return records;
+}
+async function getDetailResult(userid,recordid)
+{
+  const records= await Record.findOne({
+    where: {
+      user_id: userid,
+      record_id:recordid
+    },
+  });
+  return records;
+}
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,179 @@
 var express = require('express');
-
+const app = express();
+const { Sequelize, Model, DataTypes } = require('sequelize');
+let bodyParser = require('body-parser');
+const { token } = require('morgan');
+var jsonParser = bodyParser.json()
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const sequelize = new Sequelize('shapdatabase', 'root', 'Glassbox0128@', {
+  host: '127.0.0.1',
+  dialect: 'mysql',/* 选择 'mysql' | 'mariadb' | 'postgres' | 'mssql' 其一 */
+  timezone: '+08:00',
+  dialectOptions: {
+    dateStrings: true,
+    typeCast: true
+} 
 });
 
-module.exports = router;
+/* GET users listing. */
+app.get('/', jsonParser, function (req, res, next) {
+  res.send('users router');
+});
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  // 在这里定义模型属性
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  company_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+    // allowNull 默认为 true
+  },
+  subscribe: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  verified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  login_token: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+}, {
+  // 这是其他模型参数
+});
+
+// `sequelize.define` 会返回模型
+console.log(User === sequelize.models.user); // true
+
+function newtoken()
+{
+    var guid = "";
+    for (var i = 1; i <= 32; i++){
+      var n = Math.floor(Math.random()*16.0).toString(16);
+      guid +=   n;
+      if((i==8)||(i==12)||(i==16)||(i==20))
+        guid += "-";
+    }
+    return guid;    
+}
+app.post('/checktoken', jsonParser, async (req, res, next) => {
+  var token = req.body.params.token;
+  try {
+    const user = await User.findOne({ where: { login_token: token } });
+    if (user === null) {
+      console.log('user Not found!');
+      res.send("failed");
+    } else {
+      
+        var token=newtoken();
+        user.login_token=token;
+        await user.save();
+        req.session.user=user.toJSON()
+        res.send({result:"success",token:token,firstname:user.first_name,lastname:user.last_name,email:user.email});
+    }
+  }
+  catch (e) {
+    console.log(e)
+    res.send({result:"failed"});
+  }
+});
+app.post('/login', jsonParser, async (req, res, next) => {
+  var email = req.body.params.email;
+  var password = req.body.params.password;
+  try {
+    const user = await User.findOne({ where: { email: email } });
+    if (user === null) {
+      console.log('user Not found!');
+      res.send("failed");
+    } else {
+      if(user.password==password)
+      {
+        var token=newtoken();
+        user.login_token=token;
+        await user.save();
+        req.session.user=user.toJSON()
+        res.send({result:"success",token:token,firstname:user.first_name,lastname:user.last_name,email:user.email});
+      }
+      else
+      {
+        console.log('wrong password');
+        res.send({result:"failed"});
+      }
+     
+    }
+  }
+  catch (e) {
+    console.log(e)
+    res.send({result:"failed"});
+  }
+});
+app.post('/logout', jsonParser, async (req, res, next) => {
+  try {
+        req.session.user=null;
+        res.send({result:"success"});
+  }
+  catch (e) {
+    console.log(e)
+    res.send({result:"failed"});
+  }
+});
+app.post('/register', jsonParser, async (req, res, next) => {
+  console.log(req.body)
+  var email = req.body.params.email;
+  var password = req.body.params.password;
+  var first_name = req.body.params.first_name;
+  var last_name = req.body.params.last_name;
+  var company_name = req.body.params.company_name;
+  try {
+    const olduser = await User.findOne({ where: { email: email } });
+    if (olduser === null) {
+      console.log('user Not found!');
+      const acount = jane = await User.create({ id: 0, email: email, password: password, company_name: company_name, first_name: first_name, last_name: last_name, verified: 0 });
+    console.log(acount.toJSON())
+    res.send("success");
+    } else {
+      console.log("user has already exist!"); // true
+      res.send("duplicated");
+    }
+    
+  }
+  catch (e) {
+    console.log(e)
+    res.send("failed");
+  }
+
+});
+testdatabase()
+async function testdatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+
+module.exports = app;
